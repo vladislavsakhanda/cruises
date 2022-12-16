f4da9ed7e0ab795e855a5f2da2f033d5b5802efe
Vladislav Sakhanda <vladsakhanda@ukr.net>
2022-12-16 20:23:34 +0200

    Created Project and first Servlet

---
+++ src/main/java/com/my/Test.java
@@ -0,0 +1,17 @@
+package com.my;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Test#doGet");
+
+        resp.getWriter().println("Hello Vlad");
+    }
+}
